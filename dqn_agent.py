aa0112a1d0fdf986cc8283f256c8a7428b07b0c3
Akhiad Bercovich <akhiadber@gmail.com>
2018-08-19 19:12:25 +0000

    Double-DQN

--- dqn_agent.py
+++ dqn_agent.py
@@ -90,15 +90,15 @@ class Agent():
         states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        target_max_actions = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
         # Compute Q targets for current states 
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+        targets = rewards + (gamma * target_max_actions * (1 - dones))
 
         # Get expected Q values from local model
-        Q_expected = self.qnetwork_local(states).gather(1, actions)
+        expected = self.qnetwork_local(states).gather(1, actions)
 
         # Compute loss
-        loss = F.mse_loss(Q_expected, Q_targets)
+        loss = F.mse_loss(expected, targets)
         # Minimize the loss
         self.optimizer.zero_grad()
         loss.backward()
